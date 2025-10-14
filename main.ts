f0b30bab6e290f7d0393c5af1bfa26b0ae010804
Brendan-Orourke <brendan.orourke@stu.ocsb.ca>
2025-10-14 18:12:41 -0400

    RGB Color changing LED

--- main.ts
+++ main.ts
@@ -5,4 +5,57 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    //changes colors
+    basic.clearScreen()
+    basic.showArrow(ArrowNames.East)
+
+    //shows red                                                                        
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+
+    //shows green  
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+
+    //shows blue 
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+
+    //shows red + green
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+
+    //shows green + blue   
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+
+    //shows blue + red   
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+
+    //shows red + green + blue  
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    basic.showIcon(IconNames.Yes)
+})
