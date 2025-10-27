35719083761ef69eb3a1d4184b78ed658905ef29
Brendan-Orourke <brendan.orourke@stu.ocsb.ca>
2025-10-27 13:15:31 -0400

    RGB LED

--- main.ts
+++ main.ts
@@ -14,9 +14,9 @@ input.onButtonPressed(Button.A, function () {
     basic.showArrow(ArrowNames.East)
 
     //shows red                                                                        
-    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
     basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
 
     //shows green  
     pins.digitalWritePin(DigitalPin.P15, 1)
@@ -24,38 +24,38 @@ input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P15, 0)
 
     //shows blue 
-    pins.digitalWritePin(DigitalPin.P16, 1)
-    basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P16, 0)
-
-    //shows red + green
     pins.digitalWritePin(DigitalPin.P14, 1)
-    pins.digitalWritePin(DigitalPin.P15, 1)
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P14, 0)
+
+    //shows red + green
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
 
     //shows green + blue   
     pins.digitalWritePin(DigitalPin.P15, 1)
-    pins.digitalWritePin(DigitalPin.P16, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P15, 0)
-    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
 
     //shows blue + red   
-    pins.digitalWritePin(DigitalPin.P16, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
     basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P16, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
 
     //shows red + green + blue  
-    pins.digitalWritePin(DigitalPin.P14, 1)
+    pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
-    pins.digitalWritePin(DigitalPin.P16, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
     basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
-    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
     basic.showIcon(IconNames.Yes)
 })
